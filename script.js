a562515c04821a6c356229456e9bf04090eafcdc
bubarmovement <pivotandre03@gmail.com>
2025-03-25 22:59:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website edukasi siap digunakan!");
+});function toggleMenu() {
+    var menu = document.getElementById("nav-links");
+    menu.classList.toggle("show");
+}
